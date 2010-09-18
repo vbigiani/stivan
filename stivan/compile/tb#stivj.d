46f67817755cd984515db47f1e37a1137c3f84a9
Valerio Bigiani <vbigiani@yahoo.it>
2010-09-18 20:00:48 +0200

    works

--- stivan/compile/tb#stivj.d
+++ stivan/compile/tb#stivj.d
@@ -814,7 +814,7 @@ END
 INTERJECT_COPY_TRANS KELDOR 4 tb#StivanKeldor-4
 == TB#STIVJ IF ~IsValidForPartyDialog("tb#stiv") !StateCheck("tb#stiv",CD_STATE_NOTVALID)~ THEN
 ~L'hai fatto apposta. Dillo che l'hai fatto apposta!~
-== KELDOR ~Ti consiglio di porre un freno alla tua lingua serpentina, halfling. La tua maldicenza potrebbe costarti cara con persone meno pazienti di me.
+== KELDOR ~Ti consiglio di porre un freno alla tua lingua serpentina, halfling. La tua maldicenza potrebbe costarti cara con persone meno pazienti di me.~
 END
 
 INTERJECT_COPY_TRANS KELDORJ 11 tb#StivanKeldorj-11
